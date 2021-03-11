d8d07dca24e96e8528066cbf9774da09ca33f74c
vIv23 <vivdin3@gmail.com>
2021-03-11 11:58:41 +0300

    feat: implement 3 functions: max, min, and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+exports.min = function min(array) {
+    if (array == undefined || array.length == 0) return 0;
+    return array.sort((a, b) => a - b)[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == undefined || array.length == 0) return 0;
+    return array.sort((a, b) => a - b)[array.length - 1];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == undefined || array.length == 0) return 0;
+    return array.reduce((prev, curr) => (prev += curr)) / array.length;
+};
